ecd2cbc784dd2a58336a9c2a0845336be8ca2780
dingfude <dingfude@dingfudedeMacBook-Pro-4.local>
2018-02-20 17:39:54 +0800

    Swift4.0 实现交换方法

--- Swift_Method_Swizzling/AppDelegate.swift
+++ Swift_Method_Swizzling/AppDelegate.swift
@@ -4,7 +4,7 @@
 //
 //  Created by DFD on 2018/2/20.
 //  Copyright © 2018年 DFD. All rights reserved.
-//
+//  swift4.0 实现交换方法，只是来源 http://blog.yaoli.site/post/%E5%A6%82%E4%BD%95%E4%BC%98%E9%9B%85%E5%9C%B0%E5%9C%A8Swift4%E4%B8%AD%E5%AE%9E%E7%8E%B0Method-Swizzling
 
 import UIKit
 
@@ -13,34 +13,50 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
+
+protocol SelfAware: class {
+    static func awake()
+}
+
+class NothingToSeeHere{
+    static func harmlessFunction(){
+        let typeCount = Int(objc_getClassList(nil, 0))
+        let types = UnsafeMutablePointer<AnyClass>.allocate(capacity: typeCount)
+        let autoreleasingTypes = AutoreleasingUnsafeMutablePointer<AnyClass>(types)
+        objc_getClassList(autoreleasingTypes, Int32(typeCount))
+        for index in 0..<typeCount{
+            ((types[index]) as? SelfAware.Type)?.awake()
+        }
+    }
+}
+
+extension UIApplication {
+    private static let runOnce :Void = {
+        NothingToSeeHere.harmlessFunction()
+    }()
+    
+    override open var next: UIResponder? {
+        UIApplication.runOnce
+        return super.next
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- Swift_Method_Swizzling/ViewController.swift
+++ Swift_Method_Swizzling/ViewController.swift
@@ -15,11 +15,54 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
     }
-
-
 }
+
+extension UIViewController: SelfAware {
+    static func awake() {
+        UIViewController.classInit()
+    }
+    
+    static func classInit(){
+        swizzleMethod
+    }
+    
+    @objc func swizzled_viewWillAppear(_ animated: Bool){
+        swizzled_viewWillAppear(animated)
+        print("swizzled_viewWillAppear")
+    }
+    
+    private static let swizzleMethod : Void = {
+        let originalSelector = #selector(viewWillAppear(_:))
+        let swizzledSelector = #selector(swizzled_viewWillAppear(_:))
+        swizzlingForClass(UIViewController.self, originalSelector: originalSelector, swizzledSelector: swizzledSelector)
+    }()
+    
+    private static func swizzlingForClass(_ forClass: AnyClass, originalSelector: Selector, swizzledSelector: Selector){
+        let originalMethod = class_getInstanceMethod(forClass, originalSelector)
+        let swizzledMethod = class_getInstanceMethod(forClass, swizzledSelector)
+        
+        guard (originalMethod != nil && swizzledMethod != nil) else {
+            return
+        }
+        
+        if class_addMethod(forClass, originalSelector, method_getImplementation(swizzledMethod!), method_getTypeEncoding(swizzledMethod!)) {
+            class_replaceMethod(forClass, swizzledSelector, method_getImplementation(originalMethod!), method_getTypeEncoding(originalMethod!))
+        } else {
+            method_exchangeImplementations(originalMethod!, swizzledMethod!)
+        }
+    }
+    
+    
+}
+
+
+
+
+
+
+
 
